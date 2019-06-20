ad5c245528656db02d2bd92bc9f540bff2a8a5d1
GYU8SGH <logan@dadeco.com>
2019-06-20 13:44:25 +0800

    initial commit

---
+++ src/main/java/org/dadeco/cu996/Cu996Application.java
@@ -0,0 +1,13 @@
+package org.dadeco.cu996;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cu996Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cu996Application.class, args);
+    }
+
+}
