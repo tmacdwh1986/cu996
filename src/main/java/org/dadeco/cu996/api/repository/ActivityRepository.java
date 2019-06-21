0260090e90f891c89058cd2434adf93abc77cba9
GYU8SGH <logan@dadeco.com>
2019-06-21 15:05:23 +0800

    changes navigation page with user greeting fixes activity repository

--- src/main/java/org/dadeco/cu996/api/repository/ActivityRepository.java
+++ src/main/java/org/dadeco/cu996/api/repository/ActivityRepository.java
@@ -1,21 +1,14 @@
 package org.dadeco.cu996.api.repository;
 
 import org.dadeco.cu996.api.model.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ActivityRepository {
-    Activity findByNtAccount(String ntAccount);
-
-    Activity findByEmail(String email);
-
-    Activity findAll();
-
-    @Query(value = "", nativeQuery = true)
-    List<Object[]> getEffortByPjoname();
+public interface ActivityRepository extends JpaRepository<Activity, Integer> {
 
     Activity save(Activity productInfo);
 }
